8a2071cb989ae0fede961e8c28086c9fd967be75
Kassai Dániel <kassaidani92@gmail.com>
2025-04-14 15:23:58 +0200

    Remove greeting placeholder

--- app/src/main/java/hu/danlett/otpmobil/ui/components/HomeScreen.kt
+++ app/src/main/java/hu/danlett/otpmobil/ui/components/HomeScreen.kt
@@ -1,18 +1,44 @@
 package hu.danlett.otpmobil.ui.components
 
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import hu.danlett.otpmobil.ui.view.Greeting
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import hu.danlett.otpmobil.R
+import hu.danlett.otpmobil.ui.theme.OtpMobilAppTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        Greeting(
-            name = "Android",
-            modifier = Modifier.padding(innerPadding)
-        )
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(title = { Text(text = stringResource(R.string.app_name)) })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+
+        }
+    }
+}
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun HomeScreenPreview() {
+    OtpMobilAppTheme {
+        HomeScreen()
     }
 }
--- app/src/main/java/hu/danlett/otpmobil/ui/view/HomeActivity.kt
+++ app/src/main/java/hu/danlett/otpmobil/ui/view/HomeActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
 import hu.danlett.otpmobil.ui.components.HomeScreen
 import hu.danlett.otpmobil.ui.theme.OtpMobilAppTheme
@@ -23,21 +19,5 @@ class HomeActivity : ComponentActivity() {
                 HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OtpMobilAppTheme {
-        Greeting("Android")
     }
 }
