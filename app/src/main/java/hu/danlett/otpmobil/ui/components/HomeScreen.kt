d023bfdee8fefe9a9eeaaaea3cb31f993b1f2add
Kassai Dániel <kassaidani92@gmail.com>
2025-04-14 12:28:34 +0200

    Create home screen

---
+++ app/src/main/java/hu/danlett/otpmobil/ui/components/HomeScreen.kt
@@ -0,0 +1,18 @@
+package hu.danlett.otpmobil.ui.components
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import hu.danlett.otpmobil.ui.view.Greeting
+
+@Composable
+fun HomeScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
+}
--- app/src/main/java/hu/danlett/otpmobil/ui/view/HomeActivity.kt
+++ app/src/main/java/hu/danlett/otpmobil/ui/view/HomeActivity.kt
@@ -1,17 +1,16 @@
-package hu.danlett.otpmobil
+package hu.danlett.otpmobil.ui.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import dagger.hilt.android.AndroidEntryPoint
+import hu.danlett.otpmobil.HiltTest
+import hu.danlett.otpmobil.ui.components.HomeScreen
 import hu.danlett.otpmobil.ui.theme.OtpMobilAppTheme
 import javax.inject.Inject
 
@@ -26,12 +25,7 @@ class HomeActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             OtpMobilAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen()
             }
         }
         hiltTest.testMethod()
